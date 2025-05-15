cf00632d204e03123eee18ae9f32389dad3c3e3a
xoxoname <xoxoname@icloud.com>
2025-05-15 23:24:57 +0900

    Update constants.py

--- modules/constants.py
+++ modules/constants.py
@@ -1,10 +1,3 @@
-import os
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
-BITGET_API_KEY = os.getenv("BITGET_API_KEY")
-BITGET_API_SECRET = os.getenv("BITGET_API_SECRET")
-BITGET_API_PASSPHRASE = os.getenv("BITGET_API_PASSPHRASE")
-
-REPORT_ENDPOINT = "https://btc-daily-report.onrender.com/report"
+COINBASE_API_URL = "https://api.coinbase.com/v2/prices/spot?currency=USD"
+BITGET_API_URL = "https://api.bitget.com"
+BITGET_SYMBOL = "BTCUSDT_UMCBL"
