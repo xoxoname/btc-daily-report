36d45815c0f1b8a40b3e94aa5cf77b19d6a3da66
xoxoname <xoxoname@icloud.com>
2025-05-15 17:06:12 +0900

    Update schedule.py

--- modules/schedule.py
+++ modules/schedule.py
@@ -1,1 +1,8 @@
-# APScheduler를 이용한 정시/실시간 트리거는 여기서 구현
+# modules/schedule.py
+from apscheduler.schedulers.background import BackgroundScheduler
+from .report import build_and_send_report
+
+sched = BackgroundScheduler(timezone="UTC")
+# 예: 매일 09:00, 21:00 UTC에 리포트 발송
+sched.add_job(build_and_send_report, "cron", hour="9,21", minute=0)
+sched.start()
