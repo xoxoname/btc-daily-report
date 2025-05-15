cf42cf910ac2a80b96c406a270a5f020b275c5e1
xoxoname <xoxoname@icloud.com>
2025-05-15 20:05:46 +0900

    Delete modules directory

--- modules/schedule.py
+++
@@ -1,8 +0,0 @@
-# modules/schedule.py
-from apscheduler.schedulers.background import BackgroundScheduler
-from .report import build_and_send_report
-
-sched = BackgroundScheduler(timezone="UTC")
-# 예: 매일 09:00, 21:00 UTC에 리포트 발송
-sched.add_job(build_and_send_report, "cron", hour="9,21", minute=0)
-sched.start()
