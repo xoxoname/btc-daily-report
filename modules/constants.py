e0f59595925f8e22ead1a3478db81a1236af666a
xoxoname <xoxoname@icloud.com>
2025-05-15 23:13:03 +0900

    Update constants.py

--- modules/constants.py
+++ modules/constants.py
@@ -1,9 +1,10 @@
-# modules/constants.py
-
 import os
 
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
-BITGET_API_KEY = os.environ.get("BITGET_API_KEY")
-BITGET_API_SECRET = os.environ.get("BITGET_API_SECRET")
-BITGET_API_PASSPHRASE = os.environ.get("BITGET_API_PASSPHRASE")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+BITGET_API_KEY = os.getenv("BITGET_API_KEY")
+BITGET_API_SECRET = os.getenv("BITGET_API_SECRET")
+BITGET_API_PASSPHRASE = os.getenv("BITGET_API_PASSPHRASE")
+
+REPORT_ENDPOINT = "https://btc-daily-report.onrender.com/report"
