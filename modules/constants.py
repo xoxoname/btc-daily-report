f85ea5ff69121ed304aaa3e05ed76255647622e5
xoxoname <xoxoname@icloud.com>
2025-05-15 17:37:08 +0900

    Update constants.py

--- modules/constants.py
+++ modules/constants.py
@@ -1,13 +1,5 @@
 import os
 
-# Bitget API
-BITGET_API_KEY    = os.getenv("BITGET_API_KEY")
-BITGET_API_SECRET = os.getenv("BITGET_API_SECRET")
-BITGET_PASSPHRASE = os.getenv("BITGET_PASSPHRASE")
-
-# Telegram
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID            = os.getenv("CHAT_ID")
-
-# Server
-PORT = int(os.getenv("PORT", 5000))
+CHAT_ID = os.getenv("CHAT_ID")
+REPORT_URL = os.getenv("REPORT_URL", "https://btc-daily-report.onrender.com/report")
