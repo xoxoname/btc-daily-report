b15253749e10b73d99de541079ae31407e03a134
xoxoname <xoxoname@icloud.com>
2025-05-15 21:47:28 +0900

    Update constants.py

--- modules/constants.py
+++ modules/constants.py
@@ -1,1 +1,6 @@
-# Placeholder content for constants.py
+import os
+
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+REPORT_URL = os.environ.get("REPORT_URL")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
