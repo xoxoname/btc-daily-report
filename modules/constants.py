fdc6ece0bc286a6666a95d5c1be9344080708a6a
xoxoname <xoxoname@icloud.com>
2025-05-15 15:38:49 +0900

    Update constants.py

--- modules/constants.py
+++ modules/constants.py
@@ -1,4 +1,4 @@
-# constants.py
-TELEGRAM_BOT_TOKEN = 'your_bot_token_here'
-TELEGRAM_CHAT_ID = 1038440081
-COINBASE_API_URL = 'https://api.coinbase.com/v2/prices/spot?currency=USD'
+# 텔레그램 설정
+TELEGRAM_BOT_TOKEN = "your_bot_token"
+AUTHORIZED_CHAT_ID = 1038440081  # 사용자만 접근 가능
+TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}"
