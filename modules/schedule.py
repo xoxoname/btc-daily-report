31e6325252bf74a63476bc157d08ba8a75583abe
xoxoname <xoxoname@icloud.com>
2025-05-15 21:48:26 +0900

    Update schedule.py

--- modules/schedule.py
+++ modules/schedule.py
@@ -1,1 +1,7 @@
-# Placeholder content for schedule.py
+from apscheduler.schedulers.background import BackgroundScheduler
+from modules.report import build_and_send_report
+
+def schedule_reports():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(build_and_send_report, 'cron', hour='0,4,14', minute='30')  # KST: 9:30, 13:00, 23:00
+    scheduler.start()
