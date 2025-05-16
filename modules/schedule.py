163eab971f8539909f320ba0a227fcd7c59f9768
xoxoname <xoxoname@icloud.com>
2025-05-16 10:52:57 +0900

    Update schedule.py

--- modules/schedule.py
+++ modules/schedule.py
@@ -1,11 +1,19 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from modules.constants import REPORT_TIMES_KST
 from modules.report import build_and_send_report
+from modules.emergency import start_emergency_monitor
 import pytz
 from datetime import datetime
 
+scheduler = BackgroundScheduler(timezone=pytz.timezone("Asia/Seoul"))
+
 def start_scheduler():
-    scheduler = BackgroundScheduler(timezone=pytz.timezone("Asia/Seoul"))
-    for time in REPORT_TIMES_KST:
-        scheduler.add_job(build_and_send_report, 'cron', hour=time["hour"], minute=time["minute"])
+    for time_cfg in REPORT_TIMES_KST:
+        scheduler.add_job(
+            build_and_send_report,
+            "cron",
+            hour=time_cfg["hour"],
+            minute=time_cfg["minute"]
+        )
+    start_emergency_monitor()
     scheduler.start()
