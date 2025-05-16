09c1c86c840e8e53de9d95f74309bb8e6f5c2ecf
xoxoname <xoxoname@icloud.com>
2025-05-16 15:02:14 +0900

    Update schedule.py

--- modules/schedule.py
+++ modules/schedule.py
@@ -1,16 +1,10 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from modules.constants import REPORT_TIMES_KST
 from modules.report import build_and_send_report
 from modules.utils import get_kst_now
 
 def start_scheduler():
     scheduler = BackgroundScheduler(timezone="Asia/Seoul")
-    for t in REPORT_TIMES_KST:
-        scheduler.add_job(
-            build_and_send_report,
-            'cron',
-            hour=t['hour'],
-            minute=t['minute']
-        )
+    for hour, minute in [(9, 0), (13, 0), (23, 0)]:
+        scheduler.add_job(build_and_send_report, 'cron', hour=hour, minute=minute)
     scheduler.start()
     print("✅ 스케줄러 시작됨")
