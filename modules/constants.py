ef3ec97e931ee2470025b1654e1356c5441f0968
xoxoname <xoxoname@icloud.com>
2025-05-16 10:52:22 +0900

    Update constants.py

--- modules/constants.py
+++ modules/constants.py
@@ -9,7 +9,7 @@ BITGET_APIKEY = os.environ.get("BITGET_APIKEY")
 BITGET_APISECRET = os.environ.get("BITGET_APISECRET")
 BITGET_PASSPHRASE = os.environ.get("BITGET_PASSPHRASE")
 
-# OpenAI API
+# OpenAI
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 # 가격 급변 기준 (단위: 퍼센트)
@@ -22,5 +22,4 @@ REPORT_TIMES_KST = [
     {"hour": 23, "minute": 0},
 ]
 
-# 보고서 전송 메시지 안내
 ANALYSIS_LOADING_MESSAGE = "📡 예측 분석은 GPT 기반 외부 처리 시스템에서 수행 중입니다."
