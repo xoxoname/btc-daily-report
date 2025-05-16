433f7531b1a669c4eb1d7bae9b2f0aad599afdfe
xoxoname <xoxoname@icloud.com>
2025-05-16 09:25:35 +0900

    Update schedule.py

--- modules/schedule.py
+++ modules/schedule.py
@@ -1,12 +1,12 @@
-# modules/schedule.py
-
+import pytz
 from apscheduler.schedulers.background import BackgroundScheduler
 from modules.report import build_and_send_report
-from pytz import timezone
+from modules.constants import TIMEZONE
+
+scheduler = BackgroundScheduler(timezone=pytz.timezone(TIMEZONE))
 
 def start_scheduler():
-    scheduler = BackgroundScheduler(timezone=timezone("Asia/Seoul"))
-    scheduler.add_job(build_and_send_report, 'cron', hour=9, minute=0)
-    scheduler.add_job(build_and_send_report, 'cron', hour=13, minute=0)
-    scheduler.add_job(build_and_send_report, 'cron', hour=23, minute=0)
+    scheduler.add_job(build_and_send_report, 'cron', hour=0, minute=30)
+    scheduler.add_job(build_and_send_report, 'cron', hour=4, minute=0)
+    scheduler.add_job(build_and_send_report, 'cron', hour=14, minute=0)
     scheduler.start()
