7ccdee859b31e0805ae6f32a9de5ae9ee664c647
xoxoname <xoxoname@icloud.com>
2025-05-16 15:18:51 +0900

    Update schedule.py

--- modules/schedule.py
+++ modules/schedule.py
@@ -1,8 +1,5 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from modules.report import build_and_send_report
+ from apscheduler.schedulers.background import BackgroundScheduler
 
 def start_scheduler():
-    scheduler = BackgroundScheduler(timezone='Asia/Seoul')
-    scheduler.add_job(build_and_send_report, 'cron', hour='9,13,23', minute=0)
+    scheduler = BackgroundScheduler()
     scheduler.start()
-    print("✅ 스케줄러 시작됨")
