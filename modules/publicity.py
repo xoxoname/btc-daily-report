ce370961c7d325bd9476cb01293d0386409ac92d
xoxoname <xoxoname@icloud.com>
2025-05-18 22:15:51 +0900

    Add files via upload

--- modules/publicity.py
+++ modules/publicity.py
@@ -1,13 +1,6 @@
 import os
 import requests
-from requests.adapters import HTTPAdapter
-from urllib3.util.ssl_ import create_urllib3_context
-
-class TLSAdapter(HTTPAdapter):
-    def init_poolmanager(self, *args, **kwargs):
-        context = create_urllib3_context()
-        kwargs['ssl_context'] = context
-        return super().init_poolmanager(*args, **kwargs)
+from modules.tls_adapter import TLSAdapter
 
 session = requests.Session()
 session.mount("https://", TLSAdapter())
