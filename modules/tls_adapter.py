958ee3d962ca106dd70a7c966b5fe03b3d7db20a
xoxoname <xoxoname@icloud.com>
2025-05-18 22:41:21 +0900

    Delete modules/tls_adapter.py

--- modules/tls_adapter.py
+++
@@ -1,9 +0,0 @@
-import ssl
-from requests.adapters import HTTPAdapter
-from urllib3.poolmanager import PoolManager
-
-class TLSAdapter(HTTPAdapter):
-    def init_poolmanager(self, *args, **kwargs):
-        context = ssl._create_unverified_context()
-        kwargs['ssl_context'] = context
-        return super().init_poolmanager(*args, **kwargs)
