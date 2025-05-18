99175037b3211d8e4f9e3c7c4a4a9bc24ca0fa3d
xoxoname <xoxoname@icloud.com>
2025-05-18 20:25:32 +0900

    Add files via upload

--- src/modules/telegram.py
+++ src/modules/telegram.py
@@ -1,1 +1,18 @@
+import requests
 
+class TelegramBot:
+    def __init__(self, token, chat_id):
+        self.base_url = f'https://api.telegram.org/bot{token}'
+        self.chat_id = chat_id
+
+    def send_report(self, content):
+        requests.post(
+            f'{self.base_url}/sendMessage',
+            json={'chat_id': self.chat_id, 'text': content, 'parse_mode': 'Markdown'}
+        )
+
+    def send_alert(self, message):
+        requests.post(
+            f'{self.base_url}/sendMessage',
+            json={'chat_id': self.chat_id, 'text': f'🚨 {message}'}
+        )
