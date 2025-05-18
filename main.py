69b94456cb5c8493d19aed325d3ecd4cd79c1f4a
xoxoname <xoxoname@icloud.com>
2025-05-18 21:35:59 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import schedule
-import time
-from modules.report import generate_full_report
+from modules.report import generate_report
 from modules.telegram import send_report
 
-def job():
-    report = generate_full_report()
-    send_report(report)
-
-schedule.every(5).minutes.do(job)
-
 if __name__ == "__main__":
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    # 테스트 명령어: "/report", "/forecast", "/profit", "/schedule"
+    command = "/report"
+    report = generate_report(command)
+    send_report(report)
