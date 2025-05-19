2e51507442bb40624121a3bbc7d8cc050fd3dd01
xoxoname <xoxoname@icloud.com>
2025-05-19 20:46:00 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ import asyncio
 from modules.telegram import run_telegram_bot
 
 if __name__ == "__main__":
-    asyncio.run(run_telegram_bot())
+    try:
+        asyncio.get_event_loop().run_until_complete(run_telegram_bot())
+    except KeyboardInterrupt:
+        print("🚨 종료됨: 수동 종료 감지")
+    except Exception as e:
+        print(f"🚨 예외 발생: {e}")
