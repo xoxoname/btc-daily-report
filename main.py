450e2debd2d3193bf767957717168b5741cc5d03
xoxoname <xoxoname@icloud.com>
2025-05-19 22:23:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from scheduler.jobs import run_scheduled_jobs
-from modules.telegram import start_telegram_bot
+import threading
+from modules.telegram_bot import run_telegram_bot
+from scheduler.jobs import start_scheduler
 
 if __name__ == "__main__":
-    start_telegram_bot()
-    run_scheduled_jobs()
+    threading.Thread(target=start_scheduler, daemon=True).start()
+    run_telegram_bot()
