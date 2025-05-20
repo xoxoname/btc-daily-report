e02f63d85d4d99360d46db6cdcbee91e6a03fbe2
xoxoname <xoxoname@icloud.com>
2025-05-20 20:31:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from modules.telegram_bot import run_telegram_bot
 from modules.schedule import start_schedulers
 
 if __name__ == "__main__":
-    start_schedulers()
+    start_schedulers()   # 인자 필요 없음
     run_telegram_bot()
