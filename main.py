e4dcaa5433c83f786115a91f1ecf7d331b1f7b8d
xoxoname <xoxoname@icloud.com>
2025-05-20 15:34:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
 import nest_asyncio
-import asyncio
+nest_asyncio.apply()
+
 from modules.telegram import run_telegram_bot
 
 if __name__ == "__main__":
-    nest_asyncio.apply()
-    asyncio.run(run_telegram_bot())
+    run_telegram_bot()
