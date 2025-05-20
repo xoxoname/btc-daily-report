a2dfd1c97d7bf3727d7125b4a25cfe5e8b9a13ce
xoxoname <xoxoname@icloud.com>
2025-05-20 20:31:38 +0900

    Update schedule.py

--- modules/schedule.py
+++ modules/schedule.py
@@ -1,20 +1,20 @@
 import schedule
-import asyncio
+import threading
 from modules.telegram_bot import send_report, send_exception
 
 def job_send_report():
-    asyncio.run(send_report())
+    send_report()
 
 def job_send_exception():
-    asyncio.run(send_exception())
+    send_exception()
 
-def start_schedulers(loop):
+def start_schedulers():
     schedule.every().day.at("09:00").do(job_send_report)
     schedule.every().day.at("13:00").do(job_send_report)
     schedule.every().day.at("17:00").do(job_send_report)
     schedule.every().day.at("23:00").do(job_send_report)
     schedule.every(5).minutes.do(job_send_exception)
-    import threading
+
     def run():
         while True:
             schedule.run_pending()
