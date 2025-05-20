b5ba8dc7277843435e5e5b44b3e60d4df5843070
xoxoname <xoxoname@icloud.com>
2025-05-20 20:22:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from modules.telegram_bot import run_telegram_bot
 from modules.schedule import start_schedulers
 
 if __name__ == "__main__":
-    run_telegram_bot()      # run_polling()은 동기함수라 바로 실행
-    # start_schedulers()     # (만약 스케줄도 돌리면 아래 참고)
+    start_schedulers()
+    run_telegram_bot()
