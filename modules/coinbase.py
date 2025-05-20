912e97195bc88c4362743ab759198810668edefa
xoxoname <xoxoname@icloud.com>
2025-05-20 16:32:49 +0900

    Delete modules/coinbase.py

--- modules/coinbase.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def get_coinbase_price(symbol="BTC-USD"):
-    url = f"https://api.coinbase.com/v2/prices/{symbol}/spot"
-    r = requests.get(url)
-    r.raise_for_status()
-    return float(r.json()["data"]["amount"])
