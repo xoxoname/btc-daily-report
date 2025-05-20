c9cf37e461646b29e09a848e9dcd17e0eb2834c1
xoxoname <xoxoname@icloud.com>
2025-05-20 09:06:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from modules.telegram_bot import run_telegram_bot
+import asyncio
+from modules.telegram import run_telegram_bot
 
 if __name__ == "__main__":
-    run_telegram_bot()
+    try:
+        asyncio.run(run_telegram_bot())
+    except RuntimeError as e:
+        import nest_asyncio
+        nest_asyncio.apply()
+        loop = asyncio.get_event_loop()
+        loop.create_task(run_telegram_bot())
+        loop.run_forever()
