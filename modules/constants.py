a88d0a45a5250d24fea856e853a52ff765fbe0a5
xoxoname <xoxoname@icloud.com>
2025-05-21 16:09:20 +0900

    Delete modules directory

--- modules/constants.py
+++
@@ -1,4 +0,0 @@
-import os
-
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = int(os.getenv("TELEGRAM_CHAT_ID") or "0")
--- modules/utils.py
+++
@@ -1,5 +0,0 @@
-import pytz
-from datetime import datetime
-
-def kr_now_str():
-    return datetime.now(pytz.timezone("Asia/Seoul")).strftime("%Y-%m-%d %H:%M:%S")
