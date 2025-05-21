9257e269ec267db8a3d751e1f5519d3df044d3ec
xoxoname <xoxoname@icloud.com>
2025-05-21 11:59:22 +0900

    Update bitget_test.py

--- modules/bitget_test.py
+++ modules/bitget_test.py
@@ -1,37 +1,40 @@
 import os
+import requests
 import time
 import hmac
+import hashlib
 import base64
-import requests
 
-key = os.environ.get('BITGET_APIKEY')
-secret = os.environ.get('BITGET_APISECRET')
-passphrase = os.environ.get('BITGET_PASSPHRASE')
-
-print("BITGET_APIKEY:", repr(key))
-print("BITGET_APISECRET:", repr(secret))
-print("BITGET_PASSPHRASE:", repr(passphrase))
+API_KEY = os.environ.get("BITGET_APIKEY")
+API_SECRET = os.environ.get("BITGET_APISECRET")
+PASSPHRASE = os.environ.get("BITGET_PASSPHRASE")
 
 timestamp = str(int(time.time() * 1000))
 method = "GET"
-path = "/api/v2/mix/account/accounts"
+request_path = "/api/v2/mix/account/accounts"
+query = "productType=USDT-FUTURES"
 body = ""
-prehash = f"{timestamp}{method}{path}{body}"
-print("Prehash:", repr(prehash))
-sign = hmac.new(secret.encode(), prehash.encode(), digestmod='sha256').digest()
-signature = base64.b64encode(sign).decode()
-print("SIGNATURE:", repr(signature))
+
+pre_hash = f"{timestamp}{method}{request_path}?{query}{body}"
+
+signature = base64.b64encode(
+    hmac.new(API_SECRET.encode('utf-8'), pre_hash.encode('utf-8'), digestmod=hashlib.sha256).digest()
+).decode()
 
 headers = {
-    "ACCESS-KEY": key,
+    "ACCESS-KEY": API_KEY,
     "ACCESS-SIGN": signature,
     "ACCESS-TIMESTAMP": timestamp,
-    "ACCESS-PASSPHRASE": passphrase,
-    "Content-Type": "application/json"
+    "ACCESS-PASSPHRASE": PASSPHRASE,
+    "Content-Type": "application/json",
 }
-print("HEADERS:", headers)
 
-url = "https://api.bitget.com/api/v2/mix/account/accounts?productType=USDT-FUTURES"
-resp = requests.get(url, headers=headers)
-print("STATUS:", resp.status_code)
-print("RESPONSE:", resp.text)
+url = f"https://api.bitget.com{request_path}?{query}"
+print("pre_hash:", pre_hash)
+print("signature:", signature)
+print("headers:", headers)
+print("url:", url)
+
+response = requests.get(url, headers=headers)
+print("Status:", response.status_code)
+print("Text:", response.text)
